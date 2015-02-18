6b6517d0bd361a0ecf5ca249caa4f695d8a3598f
Pusher Robot <jack@semantic-ui.com>
2015-02-18 16:33:43 -0500

    Updated component to version 1.9.9

--- package.js
+++ package.js
@@ -5,7 +5,7 @@ var
 Package.describe({
   name    : 'semantic:ui-nag',
   summary : 'Semantic UI - Nag (official): Single component release of nag',
-  version : '1.9.0',
+  version : '1.9.9',
   git     : 'git://github.com/Semantic-Org/UI-Nag.git',
 });
 
