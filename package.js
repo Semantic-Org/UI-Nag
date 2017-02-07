1911d262f500fd26f5e80f029d482bdb282b79d2
Jack Lukic <jack@semantic-ui.com>
2017-02-07 10:57:39 -0500

    Updated component to version 2.2.7

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 Package.describe({
   name    : 'semantic:ui-nag',
   summary : 'Semantic UI - Nag: Single component release',
-  version : '2.2.6',
+  version : '2.2.7',
   git     : 'git://github.com/Semantic-Org/UI-Nag.git',
 });
 
